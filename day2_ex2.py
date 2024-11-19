c2bffd91fb30d1dd182e1764196f81ef125b19f5
alireza_astane <apoorhass@gmail.com>
2024-11-19 21:55:38 +0330

    added 1/radius as tempreture and color in plots

--- day2_ex2.py
+++ day2_ex2.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
 
-G  = 6.67430e-11
+
+# All physical prameters are in SI units
+# M represents mass
+# R represents radius
+
 M_Sun = 1.989e30
 M_Earth = 5.972e24
 M_Jupiter = 1.898e27
@@ -12,16 +16,13 @@ R_Jupyter = 69911e3
 R_Saturn = 58232e3
 R_Earth = 6371e3
 R_Sun = 696340e3
-
-
 jupyter_sun_distance = 778.57e9
 saturn_sun_distance = 1.429e12
 earth_sun_distance = 1.496e11
-
-
 earth_average_speed = 29780
 jupiter_average_speed = 13070
 saturn_average_speed = 9690
+GravitationalConstant  = 6.67430e-11
 
 class Planet: 
     def __init__(self,name,mass,position,velocity,radius):
@@ -33,7 +34,6 @@ class Planet:
         self.radius = radius
 
 class System:
-    G  = 6.67430e-11
     time_step = 60*60
     
     def __init__(self,time = 0):
@@ -47,10 +47,10 @@ class System:
 
     def callculate_force_with_sun(self,planet):
         r = np.linalg.norm(planet.position - self.Pos_Sun)
-        return -G * self.M_Sun * planet.mass / r**3 * (planet.position - self.Pos_Sun) 
+        return -GravitationalConstant * self.M_Sun * planet.mass / r**3 * (planet.position - self.Pos_Sun) 
 
     def update_planet_temp(self,planet):
-        planet.temp =  np.linalg.norm(planet.position - self.Pos_Sun)
+        planet.temp =  1/np.linalg.norm(planet.position - self.Pos_Sun)
 
     def update_planet(self,planet):
         planet.position += planet.velocity * self.time_step
@@ -63,13 +63,13 @@ class System:
             self.time = self.time + self.time_step
     
     def run(self,n):
-        trajectory = np.zeros((n,len(self.planets),4))
+        trajectory = np.zeros((n,len(self.planets),6))
         for i in tqdm(range(n)):
             self.update()
             for j,planet in enumerate(self.planets):
                 trajectory[i,j,:2] = planet.position
                 trajectory[i,j,2] = np.log(planet.radius)
-                # trajectory[i,j,3] = planet.temp
+                trajectory[i,j,3:] = planet.temp
 
         return trajectory
 
@@ -79,14 +79,22 @@ class System:
         return direction
 
     def visualize(self,trajectory):
+        plt.title("Solar System")
         plt.scatter(0,0,s=np.log(R_Sun))
         
+        colors =  (trajectory[:,:,3:] - np.min(trajectory[:,:,3:],axis=(0,1) )) / (np.max(trajectory[:,:,3:],axis=(0,1)) - np.min(trajectory[:,:,3:],axis=(0,1)))
+        print(colors)
+
         for i in range(len(self.planets)):
-            plt.scatter(trajectory[:,i,0],trajectory[:,i,1],s=trajectory[:,i,2])
+            plt.scatter(trajectory[:,i,0],trajectory[:,i,1],s=trajectory[:,i,2],c=colors[:,i])
         # for j in tqdm(range(trajectory.shape[0])):=trajectory[:,i,2]
         #     plt.scatter(trajectory[j,:,0],trajectory[j,:,1],s=trajectory[j,:,2],c=(1,0,0))
 
         plt.legend(["Sun"] + [p.name for p in self.planets])
+
+        plt.xlabel("X(m)")
+        plt.ylabel("Y(m)") 
+
         plt.show()
 
 
@@ -94,9 +102,9 @@ class System:
 solar_system = System()
 
 
-earth = Planet("Earth",M_Earth,np.array([0,earth_sun_distance]),earth_average_speed * solar_system.get_random_direction(),R_Earth)
-jupiter = Planet("Jupiter",M_Jupiter,np.array([0,earth_sun_distance]),earth_average_speed * solar_system.get_random_direction(),R_Jupyter)
-saturn = Planet("Saturn",M_Saturn,np.array([0,earth_sun_distance]),earth_average_speed * solar_system.get_random_direction(),R_Saturn)
+earth = Planet("Earth",M_Earth,np.array([earth_sun_distance,0]),earth_average_speed * solar_system.get_random_direction(),R_Earth)
+jupiter = Planet("Jupiter",M_Jupiter,np.array([earth_sun_distance,0]),earth_average_speed * solar_system.get_random_direction(),R_Jupyter)
+saturn = Planet("Saturn",M_Saturn,np.array([earth_sun_distance,0]),earth_average_speed * solar_system.get_random_direction(),R_Saturn)
 
 solar_system.add_planet(earth)
 solar_system.add_planet(jupiter)
@@ -108,10 +116,10 @@ solar_system.add_planet(saturn)
 
 trajectory = solar_system.run(365*24)
 solar_system.visualize(trajectory)
+plt.savefig("solar_system.png")
 
 
-print(R_Saturn/R_Earth, R_Jupyter/R_Earth,R_Earth/R_Earth)
-print(np.log(R_Saturn), np.log(R_Jupyter),np.log(R_Earth),np.log(R_Sun))
+
 
 
         
