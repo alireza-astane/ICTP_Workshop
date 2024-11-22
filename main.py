49be4f31794dc9597676ea6729ca876cc3b99fea
alireza_astane <apoorhass@gmail.com>
2024-11-22 20:00:52 +0330

    add test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from Solar import System, Planet
-
-# test case
-# remove type hints
-# expolodation problem
+import numpy as np
 
 
 def main():
@@ -10,25 +7,25 @@ def main():
     # M => Mass
     # R => Radius
 
-    M_Sun: float = 1.989e30
-    M_Earth: float = 5.972e24
-    M_Jupiter: float = 1.898e27
-    M_Saturn: float = 5.683e26
+    M_Sun = 1.989e30
+    M_Earth = 5.972e24
+    M_Jupiter = 1.898e27
+    M_Saturn = 5.683e26
 
-    R_Jupyter: float = 69911e3
-    R_Saturn: float = 58232e3
-    R_Earth: float = 6371e3
-    R_Sun: float = 696340e3
+    R_Jupyter = 69911e3
+    R_Saturn = 58232e3
+    R_Earth = 6371e3
+    R_Sun = 696340e3
 
-    JUPITER_SUN_DISTANCE: float = 778.57e9
-    SATURN_SUN_DISTANCE: float = 1.429e12
-    EARTH_SUN_DISTANCE: float = 1.496e11
+    JUPITER_SUN_DISTANCE = 778.57e9
+    SATURN_SUN_DISTANCE = 1.429e12
+    EARTH_SUN_DISTANCE = 1.496e11
 
-    EARTH_AVERAGE_SPEED: float = 29780.0
-    JUPITER_AVERAGE_SPEED: float = 13070.0
-    SATURN_AVERAGE_SPEED: float = 9690.0
+    EARTH_AVERAGE_SPEED = 29780.0
+    JUPITER_AVERAGE_SPEED = 13070.0
+    SATURN_AVERAGE_SPEED = 9690.0
 
-    solar_system: System = System(M_Sun, R_Sun, 0, 60 * 60)
+    solar_system = System(M_Sun, R_Sun, 0, 60 * 60)
 
     earth: Planet = Planet(
         "Earth",
@@ -61,11 +58,9 @@ def main():
     solar_system.add_planet(jupiter)
     solar_system.add_planet(saturn)
 
-    trajectory: np.ndarray = solar_system.run(30 * 365 * 24)
+    trajectory = solar_system.run(30 * 365 * 24)
 
     solar_system.visualize(trajectory, 24 * 30)
-
-    plt.savefig("solar_system.png")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,83 +1,171 @@
 import numpy as np
-import unittest
+from unittest import TestCase
 from Solar import Planet, System
 
+eps = 0.001
 
-def test_calculate_Force_and_potential(self):
+
+def almost_equal(a, b):
+    return (abs(a - b) / abs(a)) < eps
+
+
+def test_calculate_Force_and_potential():
+    planet1 = Planet(
+        "Earth",
+        5.972e24,
+        np.array([0.0, 227_940_000_000.0]),
+        np.array([0.0, 0.0]),
+        6371,
+        "o",
+    )
+    planet2 = Planet(
+        "Mars",
+        6.39e23,
+        np.array([227_940_000_000.0, 0.0]),
+        np.array([0.0, 24_077.0]),
+        3389,
+        "o",
+    )
+    system = System(1.989e30, 696_340_000, time=0, time_step=60)
+    system.add_planet(planet1)
+    system.add_planet(planet2)
+
     force, potential = system.calculate_Force_and_potential(planet1)
-    assertEqual(force.tolist(), [0, 0])
-    assertEqual(potential, 0)
+    assert almost_equal(force.tolist()[0], 1.73316655e15)
+    assert almost_equal(force.tolist()[1], -1.52587609e22)
+    assert almost_equal(potential, -3.4780823503686664e33)
 
     force, potential = system.calculate_Force_and_potential(planet2)
-    assertAlmostEqual(force[0], 1.631e20, places=1)
-    assertAlmostEqual(force[1], 0, places=1)
-    assertAlmostEqual(potential, -1.631e11, places=1)
+    assert almost_equal(force[0], -1.63267874e21)
+    assert almost_equal(force[1], 1.73316655e15)
+    assert almost_equal(potential, -3.721531874705354e32)
 
 
-def test_update_planet(self):
+def test_update_planet():
+    planet1 = Planet(
+        "Earth",
+        5.972e24,
+        np.array([0.0, 227_940_000_000.0]),
+        np.array([0.0, 0.0]),
+        6371,
+        "o",
+    )
+    planet2 = Planet(
+        "Mars",
+        6.39e23,
+        np.array([227_940_000_000.0, 0.0]),
+        np.array([0.0, 24_077.0]),
+        3389,
+        "o",
+    )
+    system = System(1.989e30, 696_340_000, time=0, time_step=60)
+    system.add_planet(planet1)
+    system.add_planet(planet2)
+
     system.update_planet(planet1)
-    assertEqual(planet1.position.tolist(), [0, 0])
-    assertEqual(planet1.velocity.tolist(), [0, 0])
-    assertEqual(planet1.temp, 0)
-    assertEqual(planet1.kinetic_Energy, 0)
-    assertEqual(planet1.potential_Energy, 0)
+    assert planet1.position.tolist() == [0.0, 227940000000.0]
+    assert planet1.velocity.tolist() == [1.7412925856903996e-08, -0.15330302292115697]
+    assert planet1.temp == 227940000000.0
+    assert planet1.kinetic_Energy == 7.017642507458052e22
+    assert planet1.potential_Energy == -3.4780823503686664e33
 
     system.update_planet(planet2)
-    assertAlmostEqual(planet2.position[0], 227_940_000_000, places=1)
-    assertAlmostEqual(planet2.position[1], 24_077, places=1)
-    assertAlmostEqual(planet2.velocity[0], 1.631e20 / (6.39e23 * 24_077) * 60, places=1)
-    assertAlmostEqual(planet2.velocity[1], 24_077, places=1)
-    assertAlmostEqual(planet2.temp, 227_940_000_000, places=1)
-    assertAlmostEqual(planet2.kinetic_Energy, 0.5 * 6.39e23 * (24_077**2), places=1)
-    assertAlmostEqual(planet2.potential_Energy, -1.631e11, places=1)
+    assert planet2.position[0] == 227940000000.0
+    assert planet2.position[1] == 1444620.0
+    assert planet2.velocity[0] == -0.1533031682391851
+    assert planet2.velocity[1] == 24076.999999191146
+    assert planet2.temp == 227940000004.57782
+    assert planet2.kinetic_Energy == 1.8521476631056448e32
+    assert planet2.potential_Energy == -3.721531874655651e32
 
 
-def test_update_system(self):
+def test_update_system():
+    planet1 = Planet(
+        "Earth",
+        5.972e24,
+        np.array([0.0, 227_940_000_000.0]),
+        np.array([0.0, 0.0]),
+        6371,
+        "o",
+    )
+    planet2 = Planet(
+        "Mars",
+        6.39e23,
+        np.array([227_940_000_000.0, 0.0]),
+        np.array([0.0, 24_077.0]),
+        3389,
+        "o",
+    )
+    system = System(1.989e30, 696_340_000, time=0, time_step=60)
+    system.add_planet(planet1)
+    system.add_planet(planet2)
+
     system.update_system()
-    assertEqual(system.time, 60)
-    assertAlmostEqual(planet1.position[0], 0, places=1)
-    assertAlmostEqual(planet1.position[1], 0, places=1)
-    assertAlmostEqual(planet1.velocity[0], 0, places=1)
-    assertAlmostEqual(planet1.velocity[1], 0, places=1)
-    assertAlmostEqual(planet1.temp, 0, places=1)
-    assertAlmostEqual(planet1.kinetic_Energy, 0, places=1)
-    assertAlmostEqual(planet1.potential_Energy, 0, places=1)
 
-    assertAlmostEqual(planet2.position[0], 227_940_000_000, places=1)
-    assertAlmostEqual(planet2.position[1], 24_077, places=1)
-    assertAlmostEqual(planet2.velocity[0], 1.631e20 / (6.39e23 * 24_077) * 60, places=1)
-    assertAlmostEqual(planet2.velocity[1], 24_077, places=1)
-    assertAlmostEqual(planet2.temp, 227_940_000_000, places=1)
-    assertAlmostEqual(planet2.kinetic_Energy, 0.5 * 6.39e23 * (24_077**2), places=1)
-    assertAlmostEqual(planet2.potential_Energy, -1.631e11, places=1)
+    assert system.time == 60
+
+    assert planet1.position.tolist() == [0.0, 227940000000.0]
+    assert planet1.velocity.tolist() == [1.7412925856903996e-08, -0.15330302292115697]
+    assert planet1.temp == 227940000000.0
+    assert planet1.kinetic_Energy == 7.017642507458052e22
+    assert planet1.potential_Energy == -3.4780823503686664e33
+
+    assert planet2.position[0] == 227940000000.0
+    assert planet2.position[1] == 1444620.0
+    assert planet2.velocity[0] == -0.1533031682391851
+    assert planet2.velocity[1] == 24076.999999191146
+    assert planet2.temp == 227940000004.57782
+    assert planet2.kinetic_Energy == 1.8521476631056448e32
+    assert planet2.potential_Energy == -3.721531874655651e32
 
 
-def test_run(self):
+def test_run():
+    planet1 = Planet(
+        "Earth",
+        5.972e24,
+        np.array([0.0, 227_940_000_000.0]),
+        np.array([0.0, 0.0]),
+        6371,
+        "o",
+    )
+    planet2 = Planet(
+        "Mars",
+        6.39e23,
+        np.array([227_940_000_000.0, 0.0]),
+        np.array([0.0, 24_077.0]),
+        3389,
+        "o",
+    )
+    system = System(1.989e30, 696_340_000, time=0, time_step=60)
+    system.add_planet(planet1)
+    system.add_planet(planet2)
+
     trajectory = system.run(10)
-    assertEqual(trajectory.shape, (10, 2, 8))
-    assertAlmostEqual(trajectory[0, 0, 0], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 1], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 2], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 3], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 4], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 5], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 6], 0, places=1)
-    assertAlmostEqual(trajectory[0, 0, 7], 0, places=1)
 
-    assertAlmostEqual(trajectory[0, 1, 0], 227_940_000_000, places=1)
-    assertAlmostEqual(trajectory[0, 1, 1], 24_077, places=1)
-    assertAlmostEqual(trajectory[0, 1, 2], 1.631e20 / (6.39e23 * 24_077) * 60, places=1)
-    assertAlmostEqual(trajectory[0, 1, 3], 24_077, places=1)
-    assertAlmostEqual(trajectory[0, 1, 4], 227_940_000_000, places=1)
-    assertAlmostEqual(trajectory[0, 1, 5], 0, places=1)
-    assertAlmostEqual(trajectory[0, 1, 6], 0.5 * 6.39e23 * (24_077**2), places=1)
-    assertAlmostEqual(trajectory[0, 1, 7], -1.631e11, places=1)
+    assert trajectory.shape == (10, 2, 8)
+    assert trajectory[0, 0, 0] == 0.0
+    assert trajectory[0, 0, 1] == 227940000000.0
+    assert trajectory[0, 0, 2] == 1.7412925856903996e-08
+    assert trajectory[0, 0, 3] == -0.15330302292115697
+    assert trajectory[0, 0, 4] == 227940000000.0
+    assert trajectory[0, 0, 5] == 60.0
+    assert trajectory[0, 0, 6] == 7.017642507458052e22
+    assert trajectory[0, 0, 7] == -3.4780823503686664e33
+
+    assert trajectory[0, 1, 0] == 227940000000.0
+    assert trajectory[0, 1, 1] == 1444620.0
+    assert trajectory[0, 1, 2] == -0.1533031682391851
+    assert trajectory[0, 1, 3] == 24076.999999191146
+    assert trajectory[0, 1, 4] == 227940000004.57782
+    assert trajectory[0, 1, 5] == 60.0
+    assert trajectory[0, 1, 6] == 1.8521476631056448e32
+    assert trajectory[0, 1, 7] == -3.721531874655651e32
 
 
 def test_get_random_direction():
     system = System(1.989e30, 696_340_000, time=0, time_step=60)
     direction = system.get_random_direction()
-    assert np.linalg.norm(direction) == 1
+    assert np.abs(np.linalg.norm(direction) - 1) < eps
 
 
 def test_add_planet():
@@ -100,5 +188,14 @@ def test_add_planet():
     assert system.planets[1].name, "Mars"
 
 
-test_get_random_direction()
-test_add_planet()
+def main():
+    test_get_random_direction()
+    test_add_planet()
+    test_calculate_Force_and_potential()
+    test_update_planet()
+    test_update_system()
+    test_run()
+
+
+if __name__ == "__main__":
+    main()
